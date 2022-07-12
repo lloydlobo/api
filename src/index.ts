2bbc122e665f630548d689a71d409f8f89cf4f36
lloydlobo <lloyd@lloydlobo.com>
2022-07-12 23:44:01 +0530

    feat: add support for xml rss feed

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
 console.log("it works");
-// #0 IMPORT DEPS
 const PORT = process.env.PORT || 8000;
-
+// #0 IMPORT DEPS
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
-
 const cors = require("cors");
 const pretty = require("pretty");
 
 const app = express();
 app.use(cors());
 
-const responses: { title: any; url: any }[] = [];
+const responses: any = [];
 
 // #1 SETUP PATH
 app.get("/", (req: any, res: any) => {
@@ -20,27 +18,34 @@ app.get("/", (req: any, res: any) => {
 });
 
 // #3 SETUP SERVER TASKS
-const url = `https://jsonplaceholder.typicode.com/`;
+const url = `https://www.theverge.com/apps/rss/index.xml`;
 
-axios.get(url).then((response: { data: any }) => {
-  const html = response.data;
-  // console.log(pretty(html));
+const getAxios = axios
+  .get(url)
+  .then((response: { data: any }) => {
+    const html = response.data; // console.log(pretty(html));
+    // #4 Use cheerio to generate
+    const $ = cheerio.load(html, { xmlMode: true }); // https://www.youtube.com/watch?v=2P92j7otn9o // https://zetcode.com/javascript/cheerio/
 
-  // #4 Use cheerio to generate
-  const $ = cheerio.load(html);
-
-  $('a:contains("sponsor")', html).each(function (this: any) {
-    const title = $(this).text().trim();
-    const url = $(this).attr("href");
-
-    // #5 push data
-    responses.push({
-      title: title,
-      url: url,
+    const cheerioData = $("entry").each(function (index: any, element: any) {
+      const title = $(element).children("title").text().trim();
+      const content = $(element).children("content").text().trim();
+      // #5 push data
+      responses.push({ title: title, content: content });
+      return { title: title, content: content };
     });
-    console.log(responses);
-  }); // end of $().each
-});
+
+    const data = cheerioData.text(); // console.log(pretty(data));
+    return data;
+  })
+  .catch(function (err: any) {
+    console.error(err);
+  });
+
+// Return the data from axios & cheerio processing
+(async () => {
+  console.log(pretty(await getAxios));
+})(); // async I F E E
 
 // #6 Display json on an endpoint
 app.get(
@@ -53,3 +58,7 @@ app.get(
 app.listen(PORT, () => {
   console.log(`server running ar http://localhost:${PORT}`);
 });
+
+// ARCHIVE
+
+// $('a:contains("user")', html).each(function (this: any) { //   const title = $(this).text().trim(); //   const url = $(this).attr("href"); //   // #5 push data // responses.push({ title: title, url: url, }); //   console.log(responses); // }); // // end of $().each
